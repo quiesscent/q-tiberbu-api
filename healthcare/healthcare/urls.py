cc8f59920484dfb42064e4cd82082211dde3660f
quiesscent <ciphedc@gmail.com>
2025-04-15 17:57:20 +0300

    urls

--- healthcare/healthcare/urls.py
+++ healthcare/healthcare/urls.py
@@ -31,17 +31,18 @@ schema_view = get_schema_view(
         license=openapi.License(name="MIT License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=(permissions.AllowAny),
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    # path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    # path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+
 
 ]
 
