cf5007954f4d3d36ad75b5e8429511ade3755606
quiesscent <ciphedc@gmail.com>
2025-04-15 17:48:23 +0300

    refactor

--- healthcare/healthcare/settings.py
+++ healthcare/healthcare/settings.py
@@ -96,9 +96,12 @@ WSGI_APPLICATION = 'healthcare.wsgi.application'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+    ),
 }
-
-
 
 # actuall database
 DATABASES = {
